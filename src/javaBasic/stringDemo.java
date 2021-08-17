be9f4a725f7c2eeaa0435cba854f2647457db0d5
yang <1413897484@qq.com>
2021-08-17 11:38:23 +0800

    0817

--- src/javaBasic/stringDemo.java
+++ src/javaBasic/stringDemo.java
@@ -1,7 +1,10 @@
 package javaBasic;
 
 public class stringDemo {
-    public void main (String[] args){
+    public static void main(String[] args) {
 
+            String str = "demo";//建立在公共池不可改变 因为底层是一个被final修饰的char数组  private final char value[];
+            String str2 = new String("demo2");//建立在堆上的普通对象
+            System.out.println("qqq".concat(str2));//concat拼接字符串其实+就行了
     }
 }
