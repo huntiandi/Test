a9b752f29ceafc89cca7f107270a101ef43c3c07
yang <1413897484@qq.com>
2021-08-30 17:38:39 +0800

    0830Company

--- src/javaBasic/IO.java
+++ src/javaBasic/IO.java
@@ -1,7 +1,6 @@
 package javaBasic;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 
 /**
  * @ProjectName: Test
@@ -20,18 +19,28 @@ public class IO {
         File ff = new File("D:\\test");
         String[] strings = ff.list();//list返回的是一级目录的文件名
         for (String s3:strings) {
-            System.out.println(s3);
+            System.out.println("list----"+s3);
         }
         File [] ssss = ff.listFiles();//listFiles获取的时目录下的所有文件和目录，想要所有的必须用这个
         for (File s:ssss) {
             if (s.isDirectory()) {
-                System.out.println(s.getName());
+                System.out.println("++++++"+s.getName());
                 File[] s1 = s.listFiles();
                 for (File s2 : s1) {
-                    System.out.println(s2);
+                    System.out.println("s2------"+s2);
                 }
             }else {
-                System.out.println(s.getName());
+                System.out.println("sssssss-"+s.getName());
+            }
+        }
+
+        //将11.txt复制到22.txt，实战中从ftp将文件复制到本地
+        try (InputStream ins = new FileInputStream("D:\\test\\11.txt"); OutputStream ous = new FileOutputStream("D:\\test\\22.txt")){
+
+            byte[] bytes = new byte[50];
+            int bs = 0;
+            while ((bs = ins.read(bytes))!= -1){
+                ous.write(bytes,0,bs);
             }
         }
     }
